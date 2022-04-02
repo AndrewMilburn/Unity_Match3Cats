036e6e8c43801520f30487df0044088340b89a68
Andrew Milburn <sidbell.069@gmail.com>
2022-04-02 09:38:21 +0100

    End #3
    Setting up grid and main camera settings to centre board

---
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] int m_height;
+    [SerializeField] int m_width;
+    Tile[,] m_grid;
+    [SerializeField] GameObject m_tile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_grid = new Tile[m_width, m_height];
+        SetUpGrid();
+    }
+
+    void SetUpGrid()
+    {
+        for(int i = 0; i < m_width; i++)
+        {
+            for (int j = 0; j < m_height; j++)
+            {
+                Vector2 tempPosn = new Vector2(i, j);
+                Instantiate(m_tile, tempPosn, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
