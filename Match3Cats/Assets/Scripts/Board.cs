4c3c222839a126e26502d82b5e34bdae2796d9e4
Andrew Milburn <sidbell.069@gmail.com>
2022-05-19 07:30:00 +0100

    Starting Again
    redoing the whole thing, changing names (and the order of row/column) to force a rethink
    
    Unity now crashing

--- Match3Cats/Assets/Scripts/Board.cs
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -3,189 +3,204 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    isWaiting,
+    isMoving
+}
+
 public class Board : MonoBehaviour
 {
-    public int m_height;
-    public int m_width;
-    Tile[,] m_grid;
-    [SerializeField] GameObject m_tile;
-    [SerializeField] GameObject[] m_pieceArray;
-    public GameObject[,] m_boardArray;
-    public float m_swapDuration;
-    public float m_checkMoveDelay;
-    int m_maxIterations = 100;
-    public float m_decreaseRowDelay;
-    public float m_RefillBoardDelay;
-    FindMatches m_FindMatches;
+    public int boardHeight;
+    public int boardWidth;
+    Tile[,] tileGrid;
+    [SerializeField] GameObject tile;
+    //[SerializeField] GameObject[] m_pieceArray;
+    //public GameObject[,] m_boardArray;
+    //public float m_swapDuration;
+    //public float m_checkMoveDelay;
+    //int m_maxIterations = 100;
+    //public float m_decreaseRowDelay;
+    //public float m_RefillBoardDelay;
+    //FindMatches m_FindMatches;
+    //public GameState m_currentState = GameState.isMoving;
 
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        m_grid = new Tile[m_width, m_height];
-        m_boardArray = new GameObject[m_width, m_height];
+        tileGrid = new Tile[boardWidth, boardHeight];
+    //    m_boardArray = new GameObject[m_width, m_height];
         SetUpGrid();
-        m_FindMatches = FindObjectOfType<FindMatches>();
+    //    m_FindMatches = FindObjectOfType<FindMatches>();
     }
 
     void SetUpGrid()
     {
-        for(int i = 0; i < m_width; i++)
+        for (int row = 0; row < boardHeight; row++)
         {
-            for (int j = 0; j < m_height; j++)
+            for (int column = 0; column < boardWidth; row++)
             {
-                Vector2 tempPosn = new Vector2(i, j);
-                GameObject gridSquare = Instantiate(m_tile, tempPosn, Quaternion.identity);
-                gridSquare.transform.parent = this.transform;
-                gridSquare.name = "Grid Square(" + i + ", " + j + ")";
-                int pieceToUse = Random.Range(0, m_pieceArray.Length);
-                int iteration = 0;
-                while(MatchesAt(i, j, m_pieceArray[pieceToUse]) && iteration < m_maxIterations)
-                {
-                    pieceToUse = Random.Range(0, m_pieceArray.Length);
-                    iteration++;
-                }
-                iteration = 0;
-                GameObject piece = Instantiate(m_pieceArray[pieceToUse], tempPosn, Quaternion.identity);
-                piece.transform.parent = this.transform;
-                piece.name = "(" + i + ", " + j + ")";
-                m_boardArray[i, j] = piece;
-            }
-        }
-    }
+                Vector2 tempPosn = new Vector2(column, row);
+                GameObject gridSquare = Instantiate(tile, tempPosn, Quaternion.identity);
+				//            gridSquare.transform.parent = this.transform;
+				//            gridSquare.name = "Grid Square(" + i + ", " + j + ")";
+				//            int pieceToUse = Random.Range(0, m_pieceArray.Length);
+				//            int iteration = 0;
+				//            while(MatchesAt(i, j, m_pieceArray[pieceToUse]) && iteration < m_maxIterations)
+				//            {
+				//                pieceToUse = Random.Range(0, m_pieceArray.Length);
+				//                iteration++;
+				//            }
+				//            iteration = 0;
+				//            GameObject piece = Instantiate(m_pieceArray[pieceToUse], tempPosn, Quaternion.identity);
+				//            piece.transform.parent = this.transform;
+				//            piece.name = "(" + i + ", " + j + ")";
+				//            m_boardArray[i, j] = piece;
+			}
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    bool MatchesAt(int column, int row, GameObject piece)
-    {
-        if(column > 1 && row > 1)
-        {
-            if(m_boardArray[column - 1, row].tag == piece.tag && m_boardArray[column - 2, row].tag == piece.tag)
-            {
-                return true;
-            }
-            if (m_boardArray[column, row - 1].tag == piece.tag && m_boardArray[column, row - 2].tag == piece.tag)
-            {
-                return true;
-            }
-        }
-        else if(column <= 1 || row <= 1)
-        {
-            if(column > 1)
-            {
-                if(m_boardArray[column - 1, row].tag == piece.tag && m_boardArray[column-2, row].tag == piece.tag)
-                {
-                    return true;
-                }
-            }
-            if(row > 1)
-            {
-                if (m_boardArray[column, row - 1].tag == piece.tag && m_boardArray[column, row - 2].tag == piece.tag)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    void DestroyMatchesAt(int column, int row)
-    {
-        if(m_boardArray[column, row].GetComponent<Piece>().m_isMatched)
-        {
-            m_FindMatches.m_currentMatches.Remove(m_boardArray[column, row]);
-            Destroy(m_boardArray[column, row]);
-            m_boardArray[column, row] = null;
-        }
-    }
-
-    public void DestroyAllMatches()
-    {
-        for (int i = 0; i < m_width; i++)
-        {
-            for (int j = 0; j < m_height; j++)
-            {
-                if(m_boardArray[i, j] != null)
-                {
-                    DestroyMatchesAt(i, j);
-                }
-            }
-        }
-        //StartCoroutine(CoDecreaseRow());
-    }
-
-    //IEnumerator CoDecreaseRow()
-    //{
-        //yield return new WaitForSeconds(m_decreaseRowDelay);
-        //int nullCount = 0;
-        //for (int i = 0; i < m_width; i++)
+        //// Update is called once per frame
+        //void Update()
         //{
-        //    for (int j = 0; j < m_height; j++)
+
+        //}
+
+        //bool MatchesAt(int column, int row, GameObject piece)
+        //{
+        //    if(column > 1 && row > 1)
         //    {
-        //        if (m_boardArray[i, j] == null)
+        //        if(m_boardArray[column - 1, row].tag == piece.tag && m_boardArray[column - 2, row].tag == piece.tag)
         //        {
-        //            nullCount++;
+        //            return true;
         //        }
-        //        else if (nullCount > 0)
+        //        if (m_boardArray[column, row - 1].tag == piece.tag && m_boardArray[column, row - 2].tag == piece.tag)
         //        {
-        //            m_boardArray[i, j].GetComponent<Piece>().m_targetPosn.y -= nullCount;
-        //            m_boardArray[i, j] = null;
+        //            return true;
         //        }
         //    }
-        //    nullCount = 0;
+        //    else if(column <= 1 || row <= 1)
+        //    {
+        //        if(column > 1)
+        //        {
+        //            if(m_boardArray[column - 1, row].tag == piece.tag && m_boardArray[column-2, row].tag == piece.tag)
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //        if(row > 1)
+        //        {
+        //            if (m_boardArray[column, row - 1].tag == piece.tag && m_boardArray[column, row - 2].tag == piece.tag)
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //    return false;
         //}
-        //StartCoroutine(CoRefillBoard());
-    //}
 
-    //void ReplenishPieces()
-    //{
-    //    for (int i = 0; i < m_width; i++)
-    //    {
-    //        for (int j = 0; j < m_height; j++)
-    //        {
-    //            if(m_boardArray[i, j] == null)
-    //            {
-    //                Vector2 tempPosn = new Vector2(i, j);
-    //                int pieceToUse = Random.Range(0, m_pieceArray.Length);
-    //                GameObject piece = Instantiate(m_pieceArray[pieceToUse], tempPosn, Quaternion.identity);
-    //                m_boardArray[i, j]= piece;
-    //            }
-    //        }
-    //    }
-    //}
+        //void DestroyMatchesAt(int column, int row)
+        //{
+        //    if(m_boardArray[column, row].GetComponent<Piece>().m_isMatched)
+        //    {
+        //        Debug.Log("Destroying " + column + ", " + row);
+        //        m_FindMatches.m_currentMatches.Remove(m_boardArray[column, row]);
+        //        Destroy(m_boardArray[column, row]);
+        //        m_boardArray[column, row] = null;
+        //    }
+        //}
 
-    //bool MatchesOnBoard()
-    //{
-    //    for (int i = 0; i < m_width; i++)
-    //    {
-    //        for (int j = 0; j < m_height; j++)
-    //        {
-    //            if(m_boardArray[i, j] != null)
-    //            {
-    //                if(m_boardArray[i, j].GetComponent<Piece>().m_isMatched)
-    //                {
-    //                    return true;
-    //                }
-    //            }
-    //        }
-    //    }
-    //    return false;
-    //}
+        //public void DestroyAllMatches()
+        //{
+        //    for (int i = 0; i < m_width; i++)
+        //    {
+        //        for (int j = 0; j < m_height; j++)
+        //        {
+        //            if(m_boardArray[i, j] != null)
+        //            {
+        //                DestroyMatchesAt(i, j);
+        //            }
+        //        }
+        //    }
+        //    StartCoroutine(CoDecreaseRow());
+        //}
 
-    //IEnumerator CoRefillBoard()
-    //{
-    //    ReplenishPieces();
-    //    yield return new WaitForSeconds(m_RefillBoardDelay);
+        //IEnumerator CoDecreaseRow()
+        //{
+        //    yield return new WaitForSeconds(m_decreaseRowDelay);
+        //    Debug.Log("In CoDecreaseRow");
+        //    int nullCount = 0;
+        //    for (int i = 0; i < m_width; i++)
+        //    {
+        //        for (int j = 0; j < m_height; j++)
+        //        {
+        //            if (m_boardArray[i, j] == null)
+        //            {
+        //                nullCount++;
+        //                Debug.Log("Null count: " + "i " + i + ", j " + j + ": " + nullCount);
+        //            }
+        //            else if (nullCount > 0)
+        //            {
+        //                Debug.Log("Decreasing " + i + ", " + j + " by " + nullCount);
+        //                m_boardArray[i, j].GetComponent<Piece>().m_targetPosn.y -= nullCount;
 
-    //    while(MatchesOnBoard())
-    //    {
-    //        yield return new WaitForSeconds(m_RefillBoardDelay);
-    //        DestroyAllMatches();
-    //    }
-    //}
+        //                m_boardArray[i, j] = null;
+        //            }
+        //        }
+        //        nullCount = 0;
+        //    }
+        //    Debug.Log("Out of CoDecreaseRow Loop");
+        //    yield return new WaitForSeconds(m_decreaseRowDelay);
+        //    Debug.Log("Out of CoDecreaseRow After Delay");
+        //    //StartCoroutine(CoRefillBoard());
+        //}
+
+        //void ReplenishPieces()
+        //{
+        //    for (int i = 0; i < m_width; i++)
+        //    {
+        //        for (int j = 0; j < m_height; j++)
+        //        {
+        //            if(m_boardArray[i, j] == null)
+        //            {
+        //                Vector2 tempPosn = new Vector2(i, j);
+        //                int pieceToUse = Random.Range(0, m_pieceArray.Length);
+        //                GameObject piece = Instantiate(m_pieceArray[pieceToUse], tempPosn, Quaternion.identity);
+        //                m_boardArray[i, j]= piece;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //bool MatchesOnBoard()
+        //{
+        //    for (int i = 0; i < m_width; i++)
+        //    {
+        //        for (int j = 0; j < m_height; j++)
+        //        {
+        //            if(m_boardArray[i, j] != null)
+        //            {
+        //                if(m_boardArray[i, j].GetComponent<Piece>().m_isMatched)
+        //                {
+        //                    return true;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //IEnumerator CoRefillBoard()
+        //{
+        //    ReplenishPieces();
+        //    yield return new WaitForSeconds(m_RefillBoardDelay);
+
+        //    while(MatchesOnBoard())
+        //    {
+        //        yield return new WaitForSeconds(m_RefillBoardDelay);
+        //        DestroyAllMatches();
+        //    }
+        //}
 
 
 }
--- Match3Cats/Assets/Scripts/FindMatches.cs
+++ Match3Cats/Assets/Scripts/FindMatches.cs
@@ -4,95 +4,95 @@ using UnityEngine;
 
 public class FindMatches : MonoBehaviour
 {
-    Board m_board;
-    public List<GameObject> m_currentMatches = new List<GameObject>();
-    [SerializeField] float m_findMatchesDelay;
+    //Board m_board;
+    //public List<GameObject> m_currentMatches = new List<GameObject>();
+    //[SerializeField] float m_findMatchesDelay;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_board = FindObjectOfType<Board>();
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    m_board = FindObjectOfType<Board>();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 
-    IEnumerator CoFindAllMatches()
-    {
-        yield return new WaitForSeconds(m_findMatchesDelay);
-        //int numMatches = 0;
+    //IEnumerator CoFindAllMatches()
+    //{
+    //    yield return new WaitForSeconds(m_findMatchesDelay);
+    //    //int numMatches = 0;
 
-        for (int i = 0; i < m_board.m_width; i++)
-        {
-            for (int j = 0; j < m_board.m_height; j++)
-            {
-                GameObject currentPiece = m_board.m_boardArray[i, j];
-                if (currentPiece != null)
-                {
-                    if(i > 0 && i < m_board.m_width - 1)
-                    {
-                        GameObject leftPiece = m_board.m_boardArray[i - 1, j];
-                        GameObject rightPiece = m_board.m_boardArray[i + 1, j];
-                        if(leftPiece != null && rightPiece != null)
-                        {
-                            if (leftPiece.tag == currentPiece.tag && rightPiece.tag == currentPiece.tag)
-                            {
-                                if (!m_currentMatches.Contains(leftPiece))
-                                {
-                                    m_currentMatches.Add(leftPiece);
-                                }
-                                leftPiece.GetComponent<Piece>().m_isMatched = true;
-                                if(!m_currentMatches.Contains(rightPiece))
-                                {
-                                    m_currentMatches.Add(rightPiece);
-                                }
-                                rightPiece.GetComponent<Piece>().m_isMatched = true;
-                                if(!m_currentMatches.Contains(currentPiece))
-                                {
-                                    m_currentMatches.Add(currentPiece);
-                                }
-                                currentPiece.GetComponent<Piece>().m_isMatched = true;
-                            }
-                        }
-                    }
-                    if(j > 0 && j < m_board.m_height - 1)
-                    {
-                        GameObject upPiece = m_board.m_boardArray[i, j + 1];
-                        GameObject downPiece = m_board.m_boardArray[i, j - 1];
-                        if(upPiece != null && downPiece != null)
-                        {
-                            if(upPiece.tag == currentPiece.tag && downPiece.tag == currentPiece.tag)
-                            {
-                                if (!m_currentMatches.Contains(upPiece))
-                                {
-                                    m_currentMatches.Add(upPiece);
-                                }
-                                upPiece.GetComponent<Piece>().m_isMatched = true;
-                                if (!m_currentMatches.Contains(downPiece))
-                                {
-                                    m_currentMatches.Add(downPiece);
-                                }
-                                downPiece.GetComponent<Piece>().m_isMatched = true;
-                                if (!m_currentMatches.Contains(currentPiece))
-                                {
-                                    m_currentMatches.Add(currentPiece);
-                                }
+    //    for (int i = 0; i < m_board.m_width; i++)
+    //    {
+    //        for (int j = 0; j < m_board.m_height; j++)
+    //        {
+    //            GameObject currentPiece = m_board.m_boardArray[i, j];
+    //            if (currentPiece != null)
+    //            {
+    //                if(i > 0 && i < m_board.m_width - 1)
+    //                {
+    //                    GameObject leftPiece = m_board.m_boardArray[i - 1, j];
+    //                    GameObject rightPiece = m_board.m_boardArray[i + 1, j];
+    //                    if(leftPiece != null && rightPiece != null)
+    //                    {
+    //                        if (leftPiece.tag == currentPiece.tag && rightPiece.tag == currentPiece.tag)
+    //                        {
+    //                            if (!m_currentMatches.Contains(leftPiece))
+    //                            {
+    //                                m_currentMatches.Add(leftPiece);
+    //                            }
+    //                            leftPiece.GetComponent<Piece>().m_isMatched = true;
+    //                            if(!m_currentMatches.Contains(rightPiece))
+    //                            {
+    //                                m_currentMatches.Add(rightPiece);
+    //                            }
+    //                            rightPiece.GetComponent<Piece>().m_isMatched = true;
+    //                            if(!m_currentMatches.Contains(currentPiece))
+    //                            {
+    //                                m_currentMatches.Add(currentPiece);
+    //                            }
+    //                            currentPiece.GetComponent<Piece>().m_isMatched = true;
+    //                        }
+    //                    }
+    //                }
+    //                if(j > 0 && j < m_board.m_height - 1)
+    //                {
+    //                    GameObject upPiece = m_board.m_boardArray[i, j + 1];
+    //                    GameObject downPiece = m_board.m_boardArray[i, j - 1];
+    //                    if(upPiece != null && downPiece != null)
+    //                    {
+    //                        if(upPiece.tag == currentPiece.tag && downPiece.tag == currentPiece.tag)
+    //                        {
+    //                            if (!m_currentMatches.Contains(upPiece))
+    //                            {
+    //                                m_currentMatches.Add(upPiece);
+    //                            }
+    //                            upPiece.GetComponent<Piece>().m_isMatched = true;
+    //                            if (!m_currentMatches.Contains(downPiece))
+    //                            {
+    //                                m_currentMatches.Add(downPiece);
+    //                            }
+    //                            downPiece.GetComponent<Piece>().m_isMatched = true;
+    //                            if (!m_currentMatches.Contains(currentPiece))
+    //                            {
+    //                                m_currentMatches.Add(currentPiece);
+    //                            }
 
-                                currentPiece.GetComponent<Piece>().m_isMatched = true;
+    //                            currentPiece.GetComponent<Piece>().m_isMatched = true;
 
-                            }
-                        }
-                    }
-                }    
-            }
-        }
-    }
+    //                        }
+    //                    }
+    //                }
+    //            }    
+    //        }
+    //    }
+    //}
 
-    public void FindAllMatches()
-    {
-        StartCoroutine(CoFindAllMatches());
-    }
+    //public void FindAllMatches()
+    //{
+    //    StartCoroutine(CoFindAllMatches());
+    //}
 }
