8a68ef957d014101ac2cc66066e2942cca6ab205
Andrew Milburn <sidbell.069@gmail.com>
2022-05-26 16:26:56 +0100

    End of L004
    Grid Initialised

--- Match3Cats/Assets/Scripts/Board.cs
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -3,32 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-public class Board : MonoBehaviour
-{
+public class Board : MonoBehaviour {
     public int boardHeight;
     public int boardWidth;
-    Tile[,] tileGrid;
+    public GameObject[] pieces;
     [SerializeField] GameObject tile;
 
-
     //Start is called before the first frame update
-    void Start()
-    {
-        tileGrid = new Tile[boardWidth, boardHeight];
+    void Start() {
         SetUpGrid();
     }
 
-    void SetUpGrid()
-    {
-        for (int row = 0; row < boardHeight; row++)
-        {
-            for (int column = 0; column < boardWidth; column++)
-            {
+    void SetUpGrid() {
+        for (int row = 0; row < boardHeight; row++) {
+            for (int column = 0; column < boardWidth; column++) {
                 Vector2 tempPosn = new Vector2(column, row);
                 GameObject gridSquare = Instantiate(tile, tempPosn, Quaternion.identity);
-			}
-		}
-	}
+                gridSquare.transform.parent = transform;
+                gridSquare.name = "Grid Cell(" + column + ", " + row + ")";
+                int pieceToUse = Random.Range(0, pieces.Length);
+                GameObject piece = Instantiate(pieces[pieceToUse], tempPosn, Quaternion.identity);
+                piece.transform.parent = transform;
+                piece.name = "Dot(" + column + ", " + row + ")";
+            }
+        }
+    }
 }
