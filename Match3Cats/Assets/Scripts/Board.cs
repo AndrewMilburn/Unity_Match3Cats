fc15661e9ea1970f756232a51018b68166f3cf50
Andrew Milburn <sidbell.069@gmail.com>
2022-05-26 17:21:29 +0100

    pieceGrid Initialised
    pieceGrid = new GameObject[boardWidth, boardHeight];

--- Match3Cats/Assets/Scripts/Board.cs
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -12,6 +12,7 @@ public class Board : MonoBehaviour {
 
     //Start is called before the first frame update
     void Start() {
+        pieceGrid = new GameObject[boardWidth, boardHeight];
         SetUpGrid();
     }
 
