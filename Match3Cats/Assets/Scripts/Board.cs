9124010c5533ef48c06089d56212ef6623704ff4
Andrew Milburn <sidbell.069@gmail.com>
2022-06-05 19:26:22 +0100

    Nearly finished Movement
    Also, braces moved back to normal.
    pieceGrid renamed to pieceArray

--- Match3Cats/Assets/Scripts/Board.cs
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -3,22 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
+public class Board : MonoBehaviour
+{
     public int boardHeight;
     public int boardWidth;
     public GameObject[] pieces;
     [SerializeField] GameObject tile;
-    public GameObject[,] pieceGrid;
+    public GameObject[,] pieceArray;
 
     //Start is called before the first frame update
-    void Start() {
-        pieceGrid = new GameObject[boardWidth, boardHeight];
+    void Start()
+    {
+        pieceArray = new GameObject[boardWidth, boardHeight];
         SetUpGrid();
     }
 
-    void SetUpGrid() {
-        for (int row = 0; row < boardHeight; row++) {
-            for (int column = 0; column < boardWidth; column++) {
+    void SetUpGrid()
+    {
+        for (int row = 0; row < boardHeight; row++)
+        {
+            for (int column = 0; column < boardWidth; column++)
+            {
                 Vector2 tempPosn = new Vector2(column, row);
                 GameObject gridSquare = Instantiate(tile, tempPosn, Quaternion.identity);
                 gridSquare.transform.parent = transform;
@@ -27,7 +32,7 @@ public class Board : MonoBehaviour {
                 GameObject piece = Instantiate(pieces[pieceToUse], tempPosn, Quaternion.identity);
                 piece.transform.parent = transform;
                 piece.name = "Dot(" + column + ", " + row + ")";
-                pieceGrid[column, row] = piece;
+                pieceArray[column, row] = piece;
                 //Debug.Log(piece.name + pieceGrid[column, row].GetComponent<Piece>().tag);
             }
         }
--- Match3Cats/Assets/Scripts/Piece.cs
+++ Match3Cats/Assets/Scripts/Piece.cs
@@ -3,18 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour {
+public class Piece : MonoBehaviour
+{ 
     Vector2 firstTouchPosn;
     Vector2 finalTouchPosn;
     float swipeAngle = 0;
+    float swipeResist = 0.5f;
     Board board;
-    int pieceCol;
-    int pieceRow;
+    [SerializeField]int pieceCol;
+    [SerializeField]int pieceRow;
     GameObject swapPiece;
-    int targetCol;
-    int targetRow;
+    [SerializeField]int targetCol;
+    [SerializeField]int targetRow;
+    Vector2 tempPosn;
+    [SerializeField] float timeToSwap = 0.5f;
+    [SerializeField] float moveTime = 0f;
 
-    private void Start() {
+
+    private void Start()
+    {
         board = FindObjectOfType<Board>();
         targetCol = (int)transform.position.x;
         targetRow = (int)transform.position.y;
@@ -22,44 +29,90 @@ public class Piece : MonoBehaviour {
         pieceRow = targetRow;
     }
 
+    private void Update()
+    {
+        if(Mathf.Abs(targetCol - transform.position.x) > 0.1f)
+        {
+            tempPosn = new Vector2(Mathf.Lerp((float)pieceCol, (float)targetCol, moveTime / timeToSwap), pieceRow);
+            transform.position = tempPosn;
+            moveTime += Time.deltaTime;
+        }
+        else
+        {
+            tempPosn = new Vector2(targetCol, targetRow);
+            transform.position = tempPosn;
+            pieceCol = targetCol;
+            board.pieceArray[pieceCol, pieceRow] = this.gameObject;
+            moveTime = 0f;
+        }
+        if (Mathf.Abs(targetRow - transform.position.y) > 0.1f)
+        {
+            tempPosn = new Vector2(pieceCol, Mathf.Lerp((float)pieceRow, (float)targetRow, moveTime / timeToSwap));
+            transform.position = tempPosn;
+            moveTime += Time.deltaTime;
+        }
+        else
+        {
+            tempPosn = new Vector2(targetCol, targetRow);
+            transform.position = tempPosn;
+            pieceRow = targetRow;
+            board.pieceArray[pieceCol, pieceRow] = this.gameObject;
+            moveTime = 0f;
+        }
+    }
 
-    private void OnMouseDown() {
+    private void OnMouseDown()
+    {
         firstTouchPosn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.Log(firstTouchPosn);
     }
 
-    private void OnMouseUp() {
+    private void OnMouseUp()
+    {
         finalTouchPosn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.Log(finalTouchPosn);
-        CalculateAngle();
+        if (Vector2.Distance(firstTouchPosn, finalTouchPosn) > swipeResist)
+        {
+            CalculateAngle();
+        }
     }
 
-    private void CalculateAngle() {
+    private void CalculateAngle()
+    {
         swipeAngle = Mathf.Atan2(finalTouchPosn.y - firstTouchPosn.y, finalTouchPosn.x - firstTouchPosn.x) * 180 / Mathf.PI;
         Debug.Log(swipeAngle);
         MovePieces();
     }
 
-    void MovePieces() {
-        if (swipeAngle > -45 && swipeAngle <= 45 && pieceCol < board.boardWidth) {  // Right Swipe
-            swapPiece = board.pieceGrid[pieceCol + 1, pieceRow];
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && pieceCol < board.boardWidth)
+        {  // Right Swipe
+            swapPiece = board.pieceArray[pieceCol + 1, pieceRow];
             swapPiece.GetComponent<Piece>().targetCol = pieceCol;
             targetCol = pieceCol + 1;
-            }
-        else if (swipeAngle > 45 && swipeAngle <= 135 && pieceRow < board.boardHeight) {  // Up Swipe
-            swapPiece = board.pieceGrid[pieceCol, pieceRow + 1];
+            moveTime = 0;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && pieceRow < board.boardHeight)
+        {  // Up Swipe
+            swapPiece = board.pieceArray[pieceCol, pieceRow + 1];
             swapPiece.GetComponent<Piece>().targetRow = pieceRow;
             targetRow = pieceRow + 1;
-            }
-        else if ((swipeAngle > 135 || swipeAngle <= -135) && pieceCol > 0) {  // Left Swipe
-            swapPiece = board.pieceGrid[pieceCol - 1, pieceRow];
+            moveTime = 0;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && pieceCol > 0)
+        {  // Left Swipe
+            swapPiece = board.pieceArray[pieceCol - 1, pieceRow];
             swapPiece.GetComponent<Piece>().targetCol = pieceCol;
             targetCol = pieceCol - 1;
-            }
-        else if (swipeAngle < -45 && swipeAngle > 135 && pieceRow > 0) {  // Down Swipe
-            swapPiece = board.pieceGrid[pieceCol, pieceRow - 1];
+            moveTime = 0;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && pieceRow > 0)
+        {  // Down Swipe
+            swapPiece = board.pieceArray[pieceCol, pieceRow - 1];
             swapPiece.GetComponent<Piece>().targetRow = pieceRow;
             targetRow = pieceRow - 1;
+            moveTime = 0;
         }
     }
 }
