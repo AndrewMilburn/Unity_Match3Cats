1cee0fe10f7780c1e7de801001be6856d8d04436
Andrew Milburn <sidbell.069@gmail.com>
2022-06-19 05:21:52 +0100

    Starting Again to find the bug in matching
    #3F Done

--- Match3Cats/Assets/Scripts/Board.cs
+++ Match3Cats/Assets/Scripts/Board.cs
@@ -13,180 +13,26 @@ public class Board : MonoBehaviour
     public GameObject[,] pieceArray;
     [SerializeField] float decreaseRowDelay;
     [SerializeField] float refillDelay;
+    Tile[,] tileGrid;
+
 
     //Start is called before the first frame update
     void Start()
     {
-        Debug.Log("In Board:Start");
-        pieceArray = new GameObject[boardWidth, boardHeight];
+        tileGrid = new Tile[boardWidth, boardHeight];
         SetUpGrid();
     }
 
     void SetUpGrid()
     {
-        Debug.Log("In Setup");
         for (int row = 0; row < boardHeight; row++)
         {
             for (int column = 0; column < boardWidth; column++)
             {
                 Vector2 tempPosn = new Vector2(column, row);
                 GameObject gridSquare = Instantiate(tile, tempPosn, Quaternion.identity);
-                gridSquare.transform.parent = transform;
-                gridSquare.name = "Grid Cell(" + column + ", " + row + ")";
-                int maxIterations = 0;
-                int pieceToUse = Random.Range(0, pieces.Length);
-                while(MatchesAt(column, row, pieces[pieceToUse]) && maxIterations < 100)
-                {
-                    pieceToUse = Random.Range(0, pieces.Length);
-                    maxIterations++;
-                    Debug.Log(maxIterations);
-                }
-                maxIterations = 0;
-                GameObject piece = Instantiate(pieces[pieceToUse], tempPosn, Quaternion.identity);
-                piece.transform.parent = transform;
-                piece.name = "Dot(" + column + ", " + row + ")";
-                pieceArray[column, row] = piece;
             }
-        }
-    }
-
-    bool MatchesAt(int col, int row, GameObject piece)
-    {
-        Debug.Log("In MatchesAt");
-        if (col > 1 && row > 1)
-        {
-            if (pieceArray[col - 1, row].tag == piece.tag && pieceArray[col - 2, row].tag == piece.tag)
-            {
-                return true;
-            }
-            if (pieceArray[col, row - 1].tag == piece.tag && pieceArray[col, row - 2].tag == piece.tag)
-            {
-                return true;
-            }
-        }
-        else if (col <= 1 || row <= 1)
-        {
-            if (col > 1)
-            {
-                if (pieceArray[col - 1, row].tag == piece.tag && pieceArray[col - 2, row].tag == piece.tag)
-                {
-                    return true;
-                }
-            }
-            if (row > 1)
-            {
-                if (pieceArray[col, row - 1].tag == piece.tag && pieceArray[col, row - 2].tag == piece.tag)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    void DestroyMatchesAt(int col, int row)
-    {
-        Debug.Log("In DestroyMatchesAt");
-
-        if (pieceArray[col, row].GetComponent<Piece>().isMatched)
-        {
-            Destroy(pieceArray[col, row]);
-            pieceArray[col, row] = null;
-        }
-    }
-
-    public void DestroyAllMatches()
-    {
-        Debug.Log("In DestroyAllMatches");
-
-        for (int row = 0; row < boardHeight; row++)
-        {
-            for (int col = 0; col < boardWidth; col++)
-            {
-                if (pieceArray[col, row] != null)
-                {
-                    DestroyMatchesAt(col, row);
-                }
-            }
-        }
-        StartCoroutine(DecreaseRow());
-    }
-
-    IEnumerator DecreaseRow()
-    {
-        Debug.Log("In DecreaseRow");
-
-        int nullCount = 0;
-        for (int col = 0; col < boardWidth; col++)
-        {
-            for (int row = 0; row < boardHeight; row++)
-            {
-                if(pieceArray[col, row] == null)
-                {
-                    nullCount++;
-                }
-                else if(nullCount > 0)
-                {
-                    pieceArray[col, row].GetComponent<Piece>().targetRow -= nullCount;
-                    pieceArray[col, row] = null;
-                }
-            }
-            nullCount = 0;
-        }
-        yield return new WaitForSeconds(decreaseRowDelay);
-        StartCoroutine(RefillBoard());
-    }
-
-    void ReplenishPieces()
-    {
-        Debug.Log("In ReplenishPieces");
-
-        for (int col = 0; col < boardWidth; col++)
-        {
-            for (int row = 0; row < boardHeight; row++)
-            {
-                if (pieceArray[col, row] == null)
-                {
-                    Vector2 tempPosn = new Vector2(col, row);
-                    int pieceToUse = Random.Range(0, pieces.Length);
-                    GameObject piece = Instantiate(pieces[pieceToUse], tempPosn, Quaternion.identity);
-                    pieceArray[col, row] = piece;
-                }
-            }
-        }
-    }
-    
-    bool MatchesOnBoard()
-    {
-        Debug.Log("In MatchesOnBoard");
-
-        for (int col = 0; col < boardWidth; col++)
-        {
-            for (int row = 0; row < boardHeight; row++)
-            {
-                if (pieceArray[col, row] != null)
-                {
-                    if(pieceArray[col, row].GetComponent<Piece>().isMatched)
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    IEnumerator RefillBoard()
-    {
-        Debug.Log("In RefillBoard");
-
-        ReplenishPieces();
-        yield return new WaitForSeconds(refillDelay);
-
-        while(MatchesOnBoard())
-        {
-            yield return new WaitForSeconds(refillDelay);
-            DestroyAllMatches();
         }
     }
 }
+
