59ab4849f60ff6ea6db4d36d31457907a78ba1df
Andrew Milburn <sidbell.069@gmail.com>
2022-06-21 06:09:25 +0100

    Halfway through 6F
    Horizontal Swipe working

--- Match3Cats/Assets/Scripts/Piece.cs
+++ Match3Cats/Assets/Scripts/Piece.cs
@@ -10,7 +10,7 @@ public class Piece : MonoBehaviour
     Vector2 firstTouchPosn;
     Vector2 finalTouchPosn;
     float swipeAngle;
-    float swipeResist;
+    float swipeResist = 0.4f;
     Board board;
     public int pieceCol;
     public int pieceRow;
@@ -28,6 +28,34 @@ public class Piece : MonoBehaviour
     int previousColumn;
     int previousRow;
 
+    private void Start()
+    {
+        Debug.Log("In Piece:Start");
+        board = FindObjectOfType<Board>();
+        targetCol = (int)transform.position.x;
+        targetRow = (int)transform.position.y;
+        pieceCol = targetCol;
+        pieceRow = targetRow;
+    }
+
+    private void Update()
+    {
+        Debug.Log("In Piece:Update");
+        if (Mathf.Abs(targetCol - transform.position.x) > 0.01f)
+        {
+            tempPosn = new Vector2(Mathf.Lerp((float)pieceCol, (float)targetCol, moveTime / timeToSwap), pieceRow);
+            transform.position = tempPosn;
+            moveTime += Time.deltaTime;
+        }
+        else
+        {
+            tempPosn = new Vector2(targetCol, targetRow);
+            transform.position = tempPosn;
+            pieceCol = targetCol;
+            board.pieceArray[pieceCol, pieceRow] = this.gameObject;
+            moveTime = 0f;
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -41,7 +69,10 @@ public class Piece : MonoBehaviour
         //Debug.Log("In Piece:OnMouseUp");
         finalTouchPosn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Debug.Log(finalTouchPosn);
-        CalculateAngle();
+        if (Vector2.Distance(firstTouchPosn, finalTouchPosn) > swipeResist)
+        {
+            CalculateAngle();
+        }
     }
 
     void CalculateAngle()
@@ -49,5 +80,39 @@ public class Piece : MonoBehaviour
         Debug.Log("In Piece:CalculateAngle");
         swipeAngle = Mathf.Atan2(finalTouchPosn.y - firstTouchPosn.y, finalTouchPosn.x - firstTouchPosn.x) * 180 / Mathf.PI;
         Debug.Log(swipeAngle);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        Debug.Log("In Piece:MovePieces");
+        if (swipeAngle > -45 && swipeAngle <= 45 && pieceCol < board.boardWidth)    // Right Swipe
+        {
+            Debug.Log("Swipe Right");
+            swapPiece = board.pieceArray[pieceCol + 1, pieceRow];
+            swapPiece.GetComponent<Piece>().targetCol = pieceCol;
+            targetCol = pieceCol + 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && pieceRow < board.boardHeight)    // Up Swipe
+        {
+            Debug.Log("Swipe Up");
+            swapPiece = board.pieceArray[pieceCol, pieceRow + 1];
+            swapPiece.GetComponent<Piece>().targetRow = pieceRow;
+            targetRow = pieceRow + 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && pieceCol > 0)    // Left Swipe
+        {
+            Debug.Log("Swipe Left");
+            swapPiece = board.pieceArray[pieceCol - 1, pieceRow];
+            swapPiece.GetComponent<Piece>().targetCol = pieceCol;
+            targetCol = pieceCol - 1;
+        }
+        else if (swipeAngle <= -45 && swipeAngle > -135 && pieceRow > 0)    // Down Swipe
+        {
+            Debug.Log("Swipe Down");
+            swapPiece = board.pieceArray[pieceCol, pieceRow - 1];
+            swapPiece.GetComponent<Piece>().targetRow = pieceRow;
+            targetRow = pieceRow - 1;
+        }
     }
 }
